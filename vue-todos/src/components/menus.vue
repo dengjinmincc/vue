71b991905e5e817ad72afc0e9f644910c4d438ac
realMin <1436563611@qq.com>
2018-02-10 17:43:17 +0800

    Add mock

--- vue-todos/src/components/menus.vue
+++ vue-todos/src/components/menus.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="list-todos">
-        <a class="list-todo activeListClass list" v-for="item in items">
+        <a @click="goList(item.id)" class="list-todo activeListClass list" :class="{'active': item.id === todoId}" v-for="item in items">
             <span class="icon-lock" v-if="item.locked"></span>
-            <span class="count-list" v-if="item.count">{{ item.count }}</span>
+            <span class="count-list" v-if="item.count > 0">{{ item.count }}</span>
             {{ item.title }}
         </a>
-        <a class="link-list-new">
+        <a class="link-list-new" @click="addTodoList">
             <span class="icon-plus"></span>
             新增
         </a>
@@ -13,14 +13,34 @@
 </template>
 
 <script>
+    import { getTodoList, addTodo } from '../api/api';
+
     export default {
         data() {
             return {
-                items: [
-                    { title: '星期一', count: 1, locked: true },
-                    { title: '星期二', count: 2, locked: true },
-                    { title: '星期三', count: 3, locked: false }
-                ]
+                items: [],
+                todoId: ''
+            };
+        },
+        created() {
+            getTodoList({}).then(res => {
+                const TODOS = res.data.todos;
+                this.items = TODOS;
+                this.todoId = TODOS[0].id;
+            });
+        },
+        methods: {
+            goList(id) {
+                this.todoId = id;
+            },
+            addTodoList() {
+                addTodo({}).then(data => {
+                    getTodoList({}).then(res => {
+                        const TODOS = res.data.todos;
+                        this.todoId = TODOS[TODOS.length - 1].id;
+                        this.items = TODOS;
+                    })
+                })
             }
         }
     }
--- vue-todos/src/main.js
+++ vue-todos/src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Mock from './mock'
+
+Mock.start()
 
 // 关闭生产模式下给出的提示
 Vue.config.productionTip = false
